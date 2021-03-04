740c6b915ed143d50db887409b9c25f3c99de07b
sahilsidana <techlife2050@gmail.com>
2021-03-04 14:03:52 -0500

    eslint

--- views/server.js
+++ views/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require('lodash');
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+app.use('/', express.static(path.resolve(__dirname, '../dist')));
+// app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../dist/index.html'));
 });
-
-app.use('/', express.static(path.resolve(__dirname, '../dist')));
 
 app.listen(3000, () => {
   console.log('App is running at port 3000');
